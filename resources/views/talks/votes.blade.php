53201739859593a18cf1b537cf7199c97b74d3c0
Burak Demirpolat <44942068+bdemirpolat@users.noreply.github.com>
2019-02-09 23:33:24 +0300

    Update votes.blade.php

--- resources/views/talks/votes.blade.php
+++ resources/views/talks/votes.blade.php
@@ -7,7 +7,7 @@
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h4><i class="fa fa-list"></i>
-                            {{ $talk }} Votes
+                            {{ $talk->title }} Votes
                         </h4>
                     </div>
                     <div class="panel-body">
